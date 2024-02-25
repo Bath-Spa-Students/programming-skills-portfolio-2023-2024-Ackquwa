eb61ad414b37e8874827be87bf2e98152441f669
Ali Farhat <ackquwa@gmail.com>
2024-02-25 15:02:55 +0400

    Exercise 4 - Three Strings to One String

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,11 @@
+# Write three strings in different variables and print the output as one string.
+
+string1 = "Dubai Universities:"
+
+string2 = "Metaverse Age,"
+
+string3 = "BathSpa University"
+
+output = f"{string1} {string2} {string3}"
+
+print(output)
