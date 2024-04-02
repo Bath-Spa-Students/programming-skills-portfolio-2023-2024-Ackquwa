134aaab8bd617432607bb1c41b53baaabf4648cf
Ali Farhat <ackquwa@gmail.com>
2024-04-02 11:41:28 +0400

    Chapter 5

--- Chapter 5- Dictionaries/Exercises/Exercise2.py
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -1,23 +1,12 @@
 glossary = {
-    'Descriptions:'
     'string': 'A series of characters.',
     'comment': 'A note in a program that the Python interpreter ignores.',
     'list': 'A collection of items in a particular order.',
     'loop': 'Work through a collection of items, one at a time.',
     'dictionary': "A collection of key-value pairs.",
-    }
+}
 
-word = 'string'
-print("\n" + word.title() + ": " + glossary[word])
+words = ['string', 'comment', 'list', 'loop', 'dictionary']
 
-word = 'comment'
-print("\n" + word.title() + ": " + glossary[word])
-
-word = 'list'
-print("\n" + word.title() + ": " + glossary[word])
-
-word = 'loop'
-print("\n" + word.title() + ": " + glossary[word])
-
-word = 'dictionary'
-print("\n" + word.title() + ": " + glossary[word])
+for word in words:
+    print("\n" + word.title() + ": " + glossary[word])
